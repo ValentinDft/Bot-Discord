22820ec3a85ecbed21ec7e1e7ecba7a58613d28a
Valentin D <vavadu07@gmail.com>
2020-06-16 01:19:02 +0200

    Test réponse à un message

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const config = require("./config.json");
 
 client.login(config.token);
 
+
+client.on('message', msg => {
+  if (msg.content === 'ca va ?') {
+    msg.reply('bien et vous ?');
+  } else if (msg.content === 'bien') {
+    msg.reply('Cool Cool Cool');
+  }
+});
