75006d5536ed34164603c76adc849db3efeba28f
Valentin D <vavadu07@gmail.com>
2020-06-16 17:28:46 +0200

    Réponse de message sur le channel

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const bot = new Discord.Client();
 const config = require("./config.json");
 
-client.login(config.token);
+bot.login(config.token);
 
-
-client.on('message', msg => {
+// Réponse du bot suivant une phrase
+bot.on('message', msg => {
   if (msg.content === 'ca va ?') {
-    msg.reply('bien et vous ?');
+    msg.channel.send('bien et vous ?');
   } else if (msg.content === 'bien') {
-    msg.reply('Cool Cool Cool');
+    msg.channel.send('Cool Cool Cool');
   }
 });
